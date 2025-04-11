3d9f5869057e0d7f5534939ad6329ceadabe4673
alfindchandra <alfindwichandra55@gmail.com>
2025-04-11 13:15:04 +0700

    baru

--- README.md
+++ README.md
@@ -36,3 +36,9 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+<h2>Halaman Web</h2>
+<img src="/public/images/joki1.png" alt="" width={50} height={50} />
+<img src="/public/images/joki2.png" alt="" width={50} height={50} />
+<img src="/public/images/joki3.png" alt="" width={50} height={50} />
+<img src="/public/images/joki4.png" alt="" width={50} height={50} />
--- src/app/Instagram.tsx
+++ src/app/Instagram.tsx
@@ -3,13 +3,13 @@ import React from "react";
 export const Instagram = () => {
   return (
     <section className="dark:bg-dark py-24">
-      <div className="relative h-40 md:h-80  bg-gradient-to-r from-pink-700 via-violet-500 to-cyan-700 md:mx-20 mx-5 rounded-xl ">
+      <div className="relative h-40 md:h-80 shadow-lg shadow-slate-600 bg-gradient-to-r from-pink-700 via-violet-500 to-cyan-700 md:mx-20 mx-5 rounded-xl ">
         <div className="container flex flex-row ">
           <div className="-mt-16 -mr-20 w-1/2">
             <img
               src="/images/hp.png"
               alt="Trading Dashboard"
-              className=" shadow-inner md:w-96 w-44"
+              className="md:w-96 w-44"
             />
           </div>
           <div className="w-3/4 mt-10 md:text-start text-end mr-8">
@@ -22,7 +22,6 @@ export const Instagram = () => {
             <button className="p-[2px] relative">
               <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-xl" />
               <div className="md:px-8 px-4 md:py-2 py-1  bg-dark rounded-xl relative group transition duration-200 text-white hover:bg-transparent">
-                {/* <span className="absolute text-xl -mt-1 -ml-8">{"<<"}</span> */}
                 {""}Lit up borders
               </div>
             </button>
